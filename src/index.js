234a9addfd374f6f2da9260e5829750b1f95f49e
Malcolm Leake <echelon1089@gmail.com>
2020-08-11 04:45:11 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("new-task-description").innerHTML += "Sorry! <code>preventDefault()</code> won't let you submit this!<br>";
-  event.preventDefault();
-}, false);
+
+  
+
+const main = document.querySelector("#main-content")
+console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const tasklist = document.querySelector("#tasks")
+
+
+taskForm.addEventListener("submit", function(e)){
+e.preventDefault()
+ console.log("Event Default")
+// have the user input
+const newTask = document.querySelector("#new-task-description")
+console.log(newTask).value
+// put it to the document with the id of tasks
+const taskItem = document.createElement("li")
+
+
+});
